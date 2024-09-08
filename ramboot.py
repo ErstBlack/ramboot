8f06bc23f82d7efc1658d32d9ab3ccf1ceedffae
ErstBlack <neasnavillus@gmail.com>
2024-09-08 18:15:30 -0400

    boot.py -> ramboot.py

--- ramboot.py
+++ ramboot.py
@@ -1,9 +1,52 @@
-from boot import boot
+from startup.initial_activations import initial_activations
+from finish.disks import hide_disks
+from setup.mounts.mounts import get_all_mounts
+from finish.move_mounts import move_system_mounts
+from finish.pivot_root import pivot_root
+from setup.ramdisk.main_ramdisk import create_ramdisk
+from setup.ramdisk.copy_mounts import copy_all_mounts
+from setup.mounts.fstab import replace_fstab
+
+import json
 
 
-def main():
-    boot()
+def boot() -> None:
+    """
+    Kickoff function to start the ramboot
 
+    Returns:
+        None
+    """
+
+    # Attempt to activate/scan filesystems
+    initial_activations()
+
+    # Get all mounts mentioned in /etc/fstab
+    all_mounts = get_all_mounts()
+
+    with open("/root/mounts.json", "a") as f:
+        json.dump([mount.__dict__ for mount in all_mounts], f, indent=2)
+
+    # Filter down to physical mounts
+    physical_mounts = all_mounts.get_physical_mounts()
+
+    # Create the ramdisk
+    ramdisk_base = create_ramdisk(physical_mounts)
+
+    # Copy mounts to the ramdisk
+    copy_all_mounts(physical_mounts, ramdisk_base)
+
+    # Fix fstab to prevent remounts
+    replace_fstab(all_mounts, ramdisk_base)
+
+    # Move dev, proc, sys, and run to ramdisk
+    move_system_mounts(ramdisk_base)
+
+    # Pivot Root
+    pivot_root(ramdisk_base)
+
+    # Hide devices used for mounts
+    hide_disks(all_mounts)
 
 if __name__ == "__main__":
-    main()
+    boot()
